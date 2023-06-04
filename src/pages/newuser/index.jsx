b5fe1655331307bd998fc44d2d5cf65e730f0114
abaldezjr <abaldezjr@gmail.com>
2023-06-03 22:57:07 -0300

    construindo formulario new user

---
+++ src/pages/newuser/index.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import LayoutDashBoard from "@/layouts/LayoutDashboard";
+
+import styles from './newuser.module.scss';
+
+export default function NewUser() {
+
+  const [definirSenhaViaEmail, setDefinirSenhaViaEmail] = useState(false);
+  const [senha, setSenha] = useState("");
+  const [confirmacaoSenha, setConfirmacaoSenha] = useState("");
+
+  const handleDefinirSenhaChange = (event) => {
+    setDefinirSenhaViaEmail(event.target.checked);
+    setSenha("");
+    setConfirmacaoSenha("");
+  };
+
+  const handleSenhaChange = (event) => {
+    setSenha(event.target.value);
+  };
+
+  const handleConfirmacaoSenhaChange = (event) => {
+    setConfirmacaoSenha(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para enviar os dados do formulário
+  };
+
+  return (
+    <>
+      <main>
+        <LayoutDashBoard>
+          <div className={styles.container}>
+            <div className={styles.topbar}>
+              <span className={styles.topbartitle}>Novo Usuário</span>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.formcontainer}>
+                <form onSubmit={handleSubmit}>
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="nome">Nome:</label>
+                    <input className={styles.forminputtext} type="text" id="nome" name="nome" required />
+                  </div>
+
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="email">Email:</label>
+                    <input className={styles.forminputtext} type="email" id="email" name="email" required />
+                  </div>
+
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="cpf">CPF:</label>
+                    <input className={styles.forminputtext} type="text" id="cpf" name="cpf" required />
+                  </div>
+
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="endereco">Endereço:</label>
+                    <input className={styles.forminputtext} type="text" id="endereco" name="endereco" required />
+                  </div>
+
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="senha">Senha:</label>
+                    <div className={styles.checkboxgroup}>
+                      <input
+                        type="checkbox"
+                        id="definirSenha"
+                        name="definirSenha"
+                        checked={definirSenhaViaEmail}
+                        onChange={handleDefinirSenhaChange}
+                      />
+                      <label className={styles.formlabel} htmlFor="definirSenha">Definir senha via email</label>
+                    </div>
+                    <input
+                      className={styles.forminputtext}
+                      type="password"
+                      id="senha"
+                      name="senha"
+                      value={senha}
+                      onChange={handleSenhaChange}
+                      required={!definirSenhaViaEmail}
+                      disabled={definirSenhaViaEmail}
+                    />
+                    <input
+                      className={styles.forminputtext}
+                      type="password"
+                      id="confirmacaoSenha"
+                      name="confirmacaoSenha"
+                      value={confirmacaoSenha}
+                      onChange={handleConfirmacaoSenhaChange}
+                      required={!definirSenhaViaEmail}
+                      disabled={definirSenhaViaEmail}
+                    />
+                  </div>
+
+                  <div className={styles.formgroup}>
+                    <label className={styles.formlabel} htmlFor="tipo">Tipo:</label>
+                    <select className={styles.forminputtext} id="tipo" name="tipo" required>
+                      <option value="">Selecione o tipo</option>
+                      <option value="1">Opção 1</option>
+                      <option value="2">Opção 2</option>
+                      <option value="3">Opção 3</option>
+                    </select>
+                  </div>
+
+                  <button className={styles.button} type="submit">Enviar</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </LayoutDashBoard>
+      </main>
+    </>
+  )
+}
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -2,6 +2,11 @@
 import LayoutDashBoard from "@/layouts/LayoutDashboard";
 import Link from 'next/link';
 
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBinLine } from 'react-icons/ri';
+
 import styles from './users.module.scss';
 
 export default function Users() {
@@ -15,9 +20,25 @@ export default function Users() {
       status: 'Ativo',
       lastPaymentDate: '01/05/2023',
     },
+    {
+      id: 2,
+      name: 'Rodrigo',
+      email: 'rodrigo@example.com',
+      cpf: '123.454.789-10',
+      status: 'Ativo',
+      lastPaymentDate: '01/06/2023',
+    },
+    {
+      id: 3,
+      name: 'Alexandre',
+      email: 'alexandre@example.com',
+      cpf: '123.454.789-10',
+      status: 'Ativo',
+      lastPaymentDate: '01/06/2023',
+    }
     // Adicione mais usuários aqui
   ];
-  
+
   return (
     <>
       <main>
@@ -56,24 +77,41 @@ export default function Users() {
               </div>
             </div>
 
-            <table>
+            <div className={styles.ctpainel}>
+              <Link href='/newuser' className={styles.btnewuser}>Novo usuário</Link>
+              <div className={styles.pagination}>
+                <Link href=''>
+                  <IoIosArrowBack />
+                </Link>
+                <span className={styles.paginationnumber}>1</span>
+                <Link href=''>
+                  <IoIosArrowForward />
+                </Link>
+              </div>
+            </div>
+
+            <table className={styles.table}>
               <thead>
-                <tr>
-                  <th>Nome</th>
-                  <th>Email</th>
-                  <th>CPF</th>
-                  <th>Status</th>
-                  <th>Data do Último Pagamento</th>
+                <tr className={styles.tr}>
+                  <th className={styles.th}>Nome</th>
+                  <th className={styles.th}>Email</th>
+                  <th className={styles.th}>CPF</th>
+                  <th className={styles.th}>Status</th>
+                  <th className={styles.th}>Data do Último Pagamento</th>
+                  <th className={styles.th}>Editar</th>
+                  <th className={styles.th}>Deletar</th>
                 </tr>
               </thead>
               <tbody>
                 {users.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.cpf}</td>
-                    <td>{user.status}</td>
-                    <td>{user.lastPaymentDate}</td>
+                  <tr key={user.id} className={styles.tr}>
+                    <td className={styles.td}>{user.name}</td>
+                    <td className={styles.td}>{user.email}</td>
+                    <td className={styles.td}>{user.cpf}</td>
+                    <td className={styles.td}>{user.status}</td>
+                    <td className={styles.td}>{user.lastPaymentDate}</td>
+                    <td className={`${styles.td} ${styles.tdcenter}`}><FaEdit/></td>
+                    <td className={`${styles.td} ${styles.tdcenter}`}><RiDeleteBinLine/></td>
                   </tr>
                 ))}
               </tbody>
