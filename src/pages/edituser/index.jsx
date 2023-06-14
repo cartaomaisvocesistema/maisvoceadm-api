1ad1fdd9527bbf53344664d9aa3932af41112cae
abaldezjr <abaldezjr@gmail.com>
2023-06-13 21:25:58 -0300

    atualizando tela edicao usuarios

--- src/pages/edituser/index.jsx
+++ src/pages/edituser/index.jsx
@@ -14,32 +14,60 @@ export default function NewUser() {
 
   const router = useRouter();
 
+  const [creditSelected, setCreditSelected] = useState(false);
+
+  const [usernameValue, setUsernameValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [cpfValue, setCpfValue] = useState('');
+  const [addressValue, setAddressValue] = useState('');
+  const [phoneValue, setPhoneValue] = useState('');
+
   const { user } = useContext(AuthContext)
-  const [userForm, setUserForm] = useState({});
 
   useEffect(() => {
     recoveryUser()
   }, [])
 
-  const handleChange = (e) => {
-    setUserForm({
-      ...userForm,
-      [e.target.name]: e.target.value
-    });
-  }
+  const handleCheckboxChange = () => {
+    setCreditSelected(!creditSelected);
+  };
 
   const recoveryUser = async () => {
     const { id } = router.query;
     const response = await api.get(`/api/usuarios/${id}`)
-    const result = (response).data;
-    setUserForm(result);
+    const data = (response).data;
+
+    if (data.username)
+      setUsernameValue(data.username);
+
+    if (data.email)
+      setEmailValue(data.email);
+
+    if (data.cpf)
+      setCpfValue(data.cpf);
+
+    if (data.address)
+      setAddressValue(data.address);
+
+    if (data.phone)
+      setPhoneValue(data.phone);
   }
 
   const updateUsuario = async (e) => {
     e.preventDefault();
-    console.log(userForm);
+
+    const { id } = router.query;
+    const updatedUser = {
+      id: id,
+      username: usernameValue,
+      email: emailValue,
+      cpf: cpfValue,
+      address: addressValue,
+      phone: phoneValue
+    }
+
     try {
-      const response = await api.patch(`/api/usuarios/`, userForm)
+      const response = await api.patch(`/api/usuarios/`, updatedUser)
       console.log(response.status);
       if (response.status === 200) {
         alert('Usuario atualizado com sucesso.');
@@ -62,28 +90,28 @@ export default function NewUser() {
             </div>
             <div className={styles.card}>
               <div className={styles.formcontainer}>
+                <div className={styles.sectiontitle}>Dados pessoais</div>
                 <form onSubmit={updateUsuario}>
                   <div className={styles.formgroup}>
                     <label className={styles.formlabel} htmlFor="nome">Nome:</label>
                     <input
-                      value={userForm.username}
                       className={styles.forminputtext}
                       type="text"
                       id="username"
                       name="username"
-                      onChange={handleChange}
+                      value={usernameValue}
+                      onChange={e => setUsernameValue(e.target.value)}
                       required />
                   </div>
-
                   <div className={styles.formgroup}>
                     <label className={styles.formlabel} htmlFor="email">Email:</label>
                     <input
-                      value={userForm.email}
                       className={styles.forminputtext}
                       type="email"
                       id="email"
                       name="email"
-                      onChange={handleChange}
+                      value={emailValue}
+                      onChange={e => setEmailValue(e.target.value)}
                       required />
                   </div>
 
@@ -94,6 +122,8 @@ export default function NewUser() {
                       type="text"
                       id="cpf"
                       name="cpf"
+                      value={cpfValue}
+                      onChange={e => setCpfValue(e.target.value)}
                       required
                     />
                   </div>
@@ -105,6 +135,8 @@ export default function NewUser() {
                       type="text"
                       id="endereco"
                       name="endereco"
+                      value={addressValue}
+                      onChange={e => setAddressValue(e.target.value)}
                       required />
                   </div>
 
@@ -115,9 +147,38 @@ export default function NewUser() {
                       type="text"
                       id="phone"
                       name="phone"
+                      value={phoneValue}
+                      onChange={e => setPhoneValue(e.target.value)}
                       required />
                   </div>
 
+                  {/*
+
+                  <div className={styles.sectiontitle}>Pagamentos</div>
+                  <div className={styles.formgrouppayment}>
+                    <div className={styles.checkboxgrouppayment}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          checked={creditSelected}
+                          onChange={handleCheckboxChange}
+                        />
+                        Habilitar Campo
+                      </label>
+                      <label className={styles.formlabel} htmlFor="phone">Telefone:</label>
+                      <input
+                        className={styles.forminputtext}
+                        type="text"
+                        id="phone"
+                        name="phone"
+                        value={phoneValue}
+                        onChange={e => setPhoneValue(e.target.value)}
+                        required
+                        disabled={!creditSelected} />
+                    </div>
+
+                  </div>
+*/}
                   <button className={styles.button} type="submit">Enviar</button>
                 </form>
               </div>
